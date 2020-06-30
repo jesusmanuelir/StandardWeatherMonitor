32c0036e80fc4dcbc85263df6bf55655a26b5409
Jesus Ibarra <jesusmanuelir@gmail.com>
2020-06-29 23:12:42 -0400

    webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react']
+              },
+            },
+          },
+          {
+            test: /\.html$/,
+            use: {
+              loader: 'html-loader',
+            },
+          },
+          {          
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+         },
+    
+        {
+          test: /\.(png|jpeg|gif|jpg)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: { name: 'assets/[hash].[ext]' },
+            }
+          ],
+        },
+        ],
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: './index.html',
+        }),
+      ],
+}
