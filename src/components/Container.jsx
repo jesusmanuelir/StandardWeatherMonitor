94e4ce1496d31a20d49a4ce65491526bb26cbb4b
Jesus Ibarra <jesusmanuelir@gmail.com>
2020-07-01 22:49:37 -0400

    API

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 
 
-const Container = () => {
+const Container = ({city,country,temp,weather}) => {
 
     return(
         <main >
-            <div className="search-box">
-                <input
-                    type        = "text"
-                    className   = "search"
-                    placeholder = "Search city ..."
 
-                />
-            </div>
             <div className="location-box">
-                <p className="location">Orlando</p>
+                <p className="location">{city}, {country}</p>
                 <p className="date">Monday 7 00 PM</p>
             </div>
             <div className="weather-icon-box">
                 <i class="wu wu-white wu-128 wu-clear"></i>
             </div>
             <div className="weather-temp-box">
-                <p className="temp">22 °c</p>
-                <p className="weather">Clouds</p>
+                <p className="temp">{Math.round(temp)} °c</p>
+                <p className="weather">{weather}</p>
             </div>
             
         </main>
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -5,11 +5,47 @@ import Container from '../components/Container'
 
 function App() {
 
-  const [location, setLocation] = useState();
+  const api = {
+    key: "85dd741130ecdb508ac9d51a039099f2",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+
+  const [search, setSearch]   = useState('');
+  const [weather, setWeather] = useState({});
+
+  const result = e => {
+    if(e.key === "Enter"){
+      fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(r => {
+        setWeather(r);
+        setSearch('');
+        console.log(r);
+        });
+    }
+  }
 
     return (
       <div>
-          <Container />
+          <div className="search-box">
+              <input
+                type        = "text"
+                className   = "search"
+                placeholder = "Search city ..."
+                onChange    = {e => setSearch(e.target.value)}
+                value       = {search}
+                onKeyPress  = {result}
+
+                />
+            </div>
+            {(typeof weather.main != "undefined") ? (
+          <Container 
+                    city    = {weather.name}
+                    country = {weather.sys.country}
+                    temp    = {weather.main.temp}
+                    weather = {weather.weather[0].main}
+          />
+            ): ('')}
       </div>
     );
   
